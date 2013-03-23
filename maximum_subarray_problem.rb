ab1a1c1c0c19e6824247c215f1bcdf9d5acbec9d
Enzo Altamiranda <enzo.alt@gmail.com>
2013-03-23 15:13:01 -0300

    Coded a draft of a linear running time solution for the maximum subarray problem.

--- maximum_subarray_problem.rb
+++ maximum_subarray_problem.rb
@@ -48,6 +48,43 @@ class Algorithms
     return max_left, max_right, left_sum + right_sum
   end
 
+  def find_maximum_subarray_linear(arr)
+    max = arr[0]
+    init_index = final_index = 0
+    minus = false
+    subarrays = []
+    (1...arr.length).each do |i|
+      if not minus and arr[i] + max >= max
+        final_index += 1
+        max += arr[i]
+      elsif arr[i] + max < max
+        minus = true
+      else
+        if arr[init_index..i].inject(:+) > max
+          final_index = i
+          max = arr[init_index..i].inject(:+)
+        else
+          subarrays << [[init_index, final_index], max]
+          init_index = final_index = i
+          max = arr[i]
+        end
+        minus = false
+      end
+    end
+    if subarrays.empty?
+      subarrays << [[init_index, final_index], max]
+    end
+    max = subarrays[0]
+    subarrays.each do |subarray|
+      if max[1] < subarray[1]
+        max = subarray
+      end
+    end
+    return subarrays
+    # left_index, right_index, sum of max subarray
+    #return max[0][0], max[0][1], max[1]
+  end
+
   def find_maximum_subarray_brute(arr)
     if arr.length == 1
       return 0, 0, arr[0]
@@ -66,7 +103,7 @@ class Algorithms
 end
 
 
-a = 100.times.map { Random.rand(40) * [1, -1][Random.rand(2)] }
+a = 15.times.map { Random.rand(40) * [1, -1][Random.rand(2)] }
 #puts "----------- testing find_maximum_crossing_subarray -----------"
 #puts a.inspect
 #puts Algorithms.new.find_maximum_subarray(a, 0, a.length-1).inspect
@@ -78,22 +115,29 @@ a = 100.times.map { Random.rand(40) * [1, -1][Random.rand(2)] }
 #puts Algorithms.new.find_maximum_subarray_brute(a).inspect
 #puts "------------------------------------------------------------\n"
 
-puts "----------- testing for fastest algorithms for input n ------------"
-(1..100).each do |i|
-  a = i.times.map { Random.rand(40) * [1, -1][Random.rand(2)] }
-  start = Time.now
-  Algorithms.new.find_maximum_subarray(a, 0, a.length - 1)
-  finish = Time.now
-  elapsed_divide = (finish - start) * (10 ** 6)
-  start = Time.now
-  Algorithms.new.find_maximum_subarray_brute(a)
-  finish = Time.now
-  elapsed_brute = (finish - start) * (10 ** 6)
-  print "n: " + i.inspect
-  print " divide_time: " + elapsed_divide.inspect + "ms"
-  print " brute_time: " + elapsed_brute.inspect + "ms"
-  print " fastest: " + ((elapsed_brute <= elapsed_divide)? "Brute": "Divide")
-  print "\n"
-end
+#puts "----------- testing for fastest algorithms for input n ------------"
+#(1..100).each do |i|
+#  a = i.times.map { Random.rand(40) * [1, -1][Random.rand(2)] }
+#  start = Time.now
+#  Algorithms.new.find_maximum_subarray(a, 0, a.length - 1)
+#  finish = Time.now
+#  elapsed_divide = (finish - start) * (10 ** 6)
+#  start = Time.now
+#  Algorithms.new.find_maximum_subarray_brute(a)
+#  finish = Time.now
+#  elapsed_brute = (finish - start) * (10 ** 6)
+#  print "n: " + i.inspect
+#  print " divide_time: " + elapsed_divide.inspect + "ms"
+#  print " brute_time: " + elapsed_brute.inspect + "ms"
+#  print " fastest: " + ((elapsed_brute <= elapsed_divide)? "Brute": "Divide")
+#  print "\n"
+#end
+#puts "------------------------------------------------------------\n"
 
+puts "----------- testing find_maximum_subarray_linear ------------"
+#a = [13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7]
+puts a.inspect
+puts Algorithms.new.find_maximum_subarray_linear(a).inspect
+puts Algorithms.new.find_maximum_subarray_brute(a).inspect
+puts Algorithms.new.find_maximum_subarray(a, 0, a.length-1).inspect
 puts "------------------------------------------------------------\n"
